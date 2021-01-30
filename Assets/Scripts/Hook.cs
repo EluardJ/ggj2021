776ff401e6d93b473e978e2f667f9a5425e06e2c
juEluard <ju.eluard@gmail.com>
2021-01-30 15:39:56 +0100

    grapple 03

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    #region Variables
+    public bool isHoldingSomething = false;
+
+    bool isActive = false;
+    #endregion
+
+    #region Unity Callbacks
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActive && !isHoldingSomething)
+            Grab(other.transform);
+    }
+    #endregion
+
+    #region Functions
+    public void Activate()
+    {
+        isActive = true;
+    }
+
+    private void Grab(Transform objectToGrab)
+    {
+        isHoldingSomething = true;
+
+        objectToGrab.parent = transform;
+    }
+    #endregion
+}
