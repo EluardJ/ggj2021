b4025477bed833dfa1cac09501687999eacb7b92
juEluard <ju.eluard@gmail.com>
2021-01-30 02:18:16 +0100

    walls generation 2

--- Assets/Scripts/Level/GridLevel.cs
+++ Assets/Scripts/Level/GridLevel.cs
@@ -23,10 +23,10 @@ public class GridLevel : MonoBehaviour
 
         elapedTimeSinceChange += Time.deltaTime;
 
-        if (elapedTimeSinceChange > 5)
+        if (elapedTimeSinceChange > 3)
         {
-            AddChunkAtRandom();
             elapedTimeSinceChange = 0;
+            AddChunkAtRandom();
         }
     }
     #endregion
@@ -36,7 +36,7 @@ public class GridLevel : MonoBehaviour
     {
         foreach (KeyValuePair<Vector2, Wall> wall in walls)
         {
-            Debug.Log(wall.Key);
+            Debug.Log("<color=green>" + wall.Key + "</color>");
         }
 
         bool horizontal = true;
@@ -63,13 +63,22 @@ public class GridLevel : MonoBehaviour
                 RoomChunk chunk = chunks[new Vector2(i, newChunk.gridCoords.y)];
                 Vector2 nextCoords = positive ? chunk.gridCoords + Vector2.left : chunk.gridCoords + Vector2.right;
                 chunk.MoveToNewPosition(nextCoords, chunkSize);
+
+                if (positive)
+                {
+                    if (i == 0)
+                        walls[nextCoords].GetUp();
+                }
+                else
+                {
+                    if (i == gridDimensions - 1)
+                        walls[nextCoords].GetUp();
+                }
             }
 
             Vector2 newCoords = positive ? newChunk.gridCoords + Vector2.left : newChunk.gridCoords + Vector2.right;
 
-            Debug.Log("new chunk : " + newChunk.gridCoords);
             walls[newChunk.gridCoords].GetUp();
-
             newChunk.MoveToNewPosition(newCoords, chunkSize);
         }
         else
@@ -81,10 +90,22 @@ public class GridLevel : MonoBehaviour
                 RoomChunk chunk = chunks[new Vector2(newChunk.gridCoords.x, i)];
                 Vector2 nextCoords = positive ? chunk.gridCoords + Vector2.down : chunk.gridCoords + Vector2.up;
                 chunk.MoveToNewPosition(nextCoords, chunkSize);
+
+                if (positive)
+                {
+                    if (i == 0)
+                        walls[nextCoords].GetUp();
+                }
+                else
+                {
+                    if (i == gridDimensions - 1)
+                        walls[nextCoords].GetUp();
+                }
             }
 
             Vector2 newCoords = positive ? newChunk.gridCoords + Vector2.down : newChunk.gridCoords + Vector2.up;
 
+            walls[newChunk.gridCoords].GetUp();
             newChunk.MoveToNewPosition(newCoords, chunkSize);
         }
     }
