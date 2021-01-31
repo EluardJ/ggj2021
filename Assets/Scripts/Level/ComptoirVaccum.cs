8b4159ba1d4d10ac5e5288abbd38ddec32ec4c2a
juEluard <ju.eluard@gmail.com>
2021-01-31 14:42:22 +0100

    comptoir fix

--- Assets/Scripts/Level/ComptoirVaccum.cs
+++ Assets/Scripts/Level/ComptoirVaccum.cs
@@ -5,10 +5,17 @@ using DG.Tweening;
 
 public class ComptoirVaccum : MonoBehaviour
 {
+    ComptoirChunk comptoir = default;
+
+    private void Awake()
+    {
+        comptoir = GetComponentInParent<ComptoirChunk>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Movable movable = other.GetComponent<Movable>();
         if (movable != null)
-            movable.PullInComptoir(transform);
+            movable.PullInComptoir(transform, comptoir);
     }
 }
